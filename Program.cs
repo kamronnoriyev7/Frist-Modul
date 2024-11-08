41fd81374b2b016be96803e4060ed74db16547a1
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-08 22:49:36 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,16 +1,104 @@
 ﻿using System;
+using System.Threading.Channels;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Frist Modul");
+        // 1
+        // Console.WriteLine("Frist Modul");
+        //
+        // //Home work 
+        // Console.Write("sonni kiriting: ");
+        // var number = Console.ReadLine();
+        // var numberlegth = number.Length;
+        // Console.Write(numberlegth);
+        // Console.WriteLine(" xonalik son");
 
-        //Home work 
-        var number = Convert.ToInt32(Console.ReadLine());
-        for (int i = 1; i < Int32.MaxValue; i *= 10)
-        {
+        // 2
+        // Console.Write("sonni kiriting: ");
+        // var number = Console.ReadLine();
+        // var firstchannel=0;
+        // for (var i = 0; i <number.Length; i++)
+        // {
+        //     var digit = Convert.ToInt32(number[i]);
+        //     if (digit % 2 == 0)
+        //     {
+        //         ++firstchannel;
+        //     }
+        //    
+        // }
+        //
+        // Console.Write(firstchannel);
+        // Console.WriteLine(" ta juft raqam bor");
 
-        }
+
+        // 3
+        // var numberchannel = 0;
+        // while (true)
+        // {
+        //     Console.Write("sonni kirit: ");
+        //     var number = Convert.ToInt32(Console.ReadLine());
+        //     if (number==0) break;
+        //     ++numberchannel;
+        // }
+        // Console.WriteLine("umumiy kiritilgan sonlar : "+numberchannel);
+
+        // 4
+        // var sumdigit = 0;
+        // while (true)
+        // {
+        //     Console.Write("sonni kirit: ");
+        //     var number = Convert.ToInt32(Console.ReadLine());
+        //     if (number==0) break;
+        //     if (number > 50)
+        //     {
+        //         sumdigit += number;
+        //     }
+        //   
+        // }
+        // Console.WriteLine("50 dan katta sonlar : "+sumdigit);
+
+        // 5
+        // var sum = 0;
+        // Console.Write("sonni kiriting: ");
+        // var numberString = Console.ReadLine();
+        // var numberInt = Convert.ToInt32(numberString);
+        // for (int i = 0; i < numberString.Length; i++)
+        // {
+        //     if (numberString[i]=='5')
+        //     {
+        //         ++sum;
+        //     }
+        // }
+        //
+        // if (sum>0)
+        // {
+        //     Console.WriteLine("5 raqami bor");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("5 raqami yoq");
+        // }
+
+        // 6
+        // var digit3number = 1;
+        // Console.Write("sonni kiriting: ");
+        // var number = Convert.ToInt32(Console.ReadLine());
+        // while (digit3number < number)
+        // {
+        //     digit3number *= 3;
+        // }
+        // if (digit3number == number)
+        // {
+        //     Console.WriteLine("3 ning darajasi ");
+        //     
+        // }
+        // else
+        // {
+        //     Console.WriteLine("3 ning darajasi emas ");
+        //    
+        // }
+        // Main();
     }
 }
