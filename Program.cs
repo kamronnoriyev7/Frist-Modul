af38297a1c04c7867a064271d821a1d5a268859c
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-08 20:55:58 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Frist Modul");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Frist Modul");
+
+        //Home work 
+        var number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < Int32.MaxValue; i *= 10)
+        {
+
+        }
+    }
+}
